c8fe7063125f4b25caa6c9c149da54bdb2c02091
Richard Horrocks <richardjhorrocks@gmail.com>
2017-07-30 17:35:42 +0100

    Addition of socket.io.

---
+++ traversy-media/socketio/socketio-mongodb-chat-app/server.js
@@ -0,0 +1,64 @@
+const mongo = require("mongodb").MongoClient;
+const client = require("socket.io").listen(4000).sockets;
+
+// Connect to MongoDB.
+mongo.connect("mongodb://127.0.0.1/mongochat", function(err, db) {
+  if (err) {
+    throw err;
+  }
+
+  console.log("MongoDB connected.");
+
+  // Connect to socket.io.
+  client.on("connection", function() {
+    let chat = db.collection('chats');
+
+    // Send the status to the client.
+    sendStatus = function(s) {
+      socket.emit('status', s);
+    }
+
+    // Get chats from Mongo collection.
+    chat.find().limit(100).sort({_id: 1}).
+      toArray(function(err, res) {
+      if (err) {
+        throw err;
+      }
+
+      // Emit messages.
+      socket.emit('output', res);
+    });
+
+    // Handle input events.
+    socket.on('input', function(data) {
+      let name = data.name;
+      let message = data.message;
+
+      // Check for name and message.
+      if (name == '' || message == '') {
+        // Send error status.
+        sendStatus("Please enter a name and message.");
+      } else {
+        // Insert message.
+        chat.insert({name: name, message: message}, function() {
+          client.emit('output', [data]);
+
+          // Send status object.
+          sendStatus({
+            message: 'Message sent',
+            clear: true
+          });
+        });
+      }
+    });
+
+    // Handle clear.
+    socket.on('clear', function(data) {
+      // Remove all chats from the collection.
+      chat.remove({}, function() {
+        // Emit cleared.
+        socket.emit('Cleared');
+      });
+    });
+  });
+});
