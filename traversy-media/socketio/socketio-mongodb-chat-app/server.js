ee2884fef17eda9ed38f513e67ab88eb575fb2fb
Richard Horrocks <richardjhorrocks@gmail.com>
2017-08-07 12:33:39 +0100

    Socket.io completion.

--- traversy-media/socketio/socketio-mongodb-chat-app/server.js
+++ traversy-media/socketio/socketio-mongodb-chat-app/server.js
@@ -10,7 +10,7 @@ mongo.connect("mongodb://127.0.0.1/mongochat", function(err, db) {
   console.log("MongoDB connected.");
 
   // Connect to socket.io.
-  client.on("connection", function() {
+  client.on("connection", function(socket) {
     let chat = db.collection('chats');
 
     // Send the status to the client.
