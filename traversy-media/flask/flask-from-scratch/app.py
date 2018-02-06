5152c60545aa9c29a9f2c87ccbfc0b4f98e2cf60
Richard Horrocks <richardjhorrocks@gmail.com>
2018-02-06 19:53:37 +0000

    More on registration

--- traversy-media/flask/flask-from-scratch/app.py
+++ traversy-media/flask/flask-from-scratch/app.py
@@ -38,6 +38,7 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
+        return render_template('register.html', form=form)
 
     return render_template('register.html', form=form)
 
