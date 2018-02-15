ccd18e59cd3445cc270dc382be59eb0a20281308
Richard Horrocks <richardjhorrocks@gmail.com>
2018-02-15 10:34:00 +0000

    Completion

--- traversy-media/javascript/javascript-reddit-search/index.js
+++ traversy-media/javascript/javascript-reddit-search/index.js
@@ -12,19 +12,41 @@ searchForm.addEventListener('submit', e => {
     // Get limit.
     const searchLimit = document.getElementById('limit').value;
 
-    console.log(searchTerm);
     // Check form isn't empty.
     if (!searchTerm) {
         // Show a message.
-        console.log("empty");
-        //showMessage('Please add a search term', 'alert-danger');
+        showMessage('Please add a search term', 'alert-danger');
     }
 
     // Clear input.
     searchInput.value = '';
 
     // Search Reddit.
-    //reddit.search(searchTerm, searchLimit, sortBy);
+    reddit.search(searchTerm, searchLimit, sortBy)
+        .then(results => {
+            let output = '<div class="card-columns">';
+            // Loop through posts.
+            results.forEach(post => {
+                // Check for image.
+                let image = post.preview ? post.preview.images[0].source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+
+                output += `
+                    <div class="card">
+                        <img class="card-img-top" src="${image}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                            <p class="card-text">${truncateText(post.selftext, 100)}</p>
+                            <a href="${post.url}" target="_blank" class="btn btn-primary">Read More</a>
+                            <hr>
+                            <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+                            <span class="badge badge-dark">Score: ${post.score}</span>
+                        </div>
+                    </div>
+                `;
+            });
+            output += '</dev>';
+            document.getElementById('results').innerHTML = output;
+        });
 
     e.preventDefault();
 });
@@ -47,4 +69,14 @@ function showMessage(message, className) {
 
     // Timeout alert.
     setTimeout(() => document.querySelector('.alert').remove(), 3000 );
+}
+
+// Truncate text.
+function truncateText(text, limit) {
+    const shortened = text.indexOf(' ', limit);
+    if (shortened == -1) {
+        return text;
+    } else {
+        return text.substring(0, shortened);
+    }
 }
