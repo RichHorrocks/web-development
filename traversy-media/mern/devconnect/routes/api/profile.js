ea6e44d7087c22e409ffc3b69cfeadcbf5d43c95
Richard Horrocks <richardjhorrocks@gmail.com>
2018-05-26 15:51:28 +0100

    Update

--- traversy-media/mern/devconnect/routes/api/profile.js
+++ traversy-media/mern/devconnect/routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile and User models
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -17,7 +20,9 @@ router.get('/test', (req, res) => res.json({ msg: 'Profile works' }));
 // @access Private
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const errors = {};
+
   Profile.findOne({ user: req.user.id })
+    .populate('user', ['name', 'avatar'])
     .then(profile => {
       if (!profile) {
         errors.noprofile = 'There is no profile for this user';
@@ -33,6 +38,14 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @desc   Create or editcurrent user's profile
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateProfileInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    // Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
   // Get fields
   const profileFields = {};
   profileFields.user = req.user.id;
