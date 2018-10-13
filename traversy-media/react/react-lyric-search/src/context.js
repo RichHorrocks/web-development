ed23d474e2951e5bebfad43fb769b7ec4d15232b
Richard Horrocks <richardjhorrocks@gmail.com>
2018-10-13 22:17:54 +0100

    Lyrics

--- traversy-media/react/react-lyric-search/src/context.js
+++ traversy-media/react/react-lyric-search/src/context.js
@@ -9,13 +9,14 @@ export class Provider extends Component {
     heading: 'Top 10 Tracks'
   }
 
-  componentDidMount() {
-    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=uk&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
-      .then(res => {
-        console.log(res.data);
-        this.setState({track_list: res.data.message.body.track_list});
-      })
-      .catch(err => console.log(err));
+  async componentDidMount() {
+    try {
+      const res = await axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=uk&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`);
+
+      this.setState({ track_list: res.data.message.body.track_list });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render () {
