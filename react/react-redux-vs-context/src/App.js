b8d680e0f781e291747db7140fecd953dfdb5ab6
Richard Horrocks <richardjhorrocks@gmail.com>
2019-05-08 17:05:12 +0100

    Redux converted to Context API

---
+++ react/react-redux-vs-context/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import ProductsPage from './pages/Products';
+import CartPage from './pages/Cart';
+
+import GlobalState from './context/GlobalState';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <GlobalState>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={ProductsPage} exact />
+            <Route path="/cart" component={CartPage} exact />
+          </Switch>
+        </BrowserRouter>
+      </GlobalState>
+    );
+  }
+}
+
+export default App;
