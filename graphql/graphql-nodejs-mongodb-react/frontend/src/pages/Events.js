6a9b2bf3d0bab6b055eef658b6bb1e1394ca4f38
Richard Horrocks <richardjhorrocks@gmail.com>
2019-06-03 12:20:04 +0100

    Addition of modal.

--- graphql/graphql-nodejs-mongodb-react/frontend/src/pages/Events.js
+++ graphql/graphql-nodejs-mongodb-react/frontend/src/pages/Events.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop.js';
 
 import './events.css';
 
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  createEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  cancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  confirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
     return (
-      <div className="events-control">
-        <p>Share your own events!</p>
-        <button className="btn">Create Event</button>
-      </div>
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.cancelHandler}
+            onConfirm={this.confirmHandler}
+          >
+            Content
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own events!</p>
+          <button className="btn" onClick={this.createEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
     );
   }
 }
