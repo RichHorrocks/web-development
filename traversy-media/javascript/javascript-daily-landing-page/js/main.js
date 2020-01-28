560d9eda360f46160f35205699a73d5be00e9b67
Richard Horrocks <richardjhorrocks@gmail.com>
2020-01-28 09:40:06 +0000

    Complete app.

--- traversy-media/javascript/javascript-daily-landing-page/js/main.js
+++ traversy-media/javascript/javascript-daily-landing-page/js/main.js
@@ -30,5 +30,81 @@ function addZero(n) {
   return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
+// Set background picture and greeting.
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage = "url('../img/morning.jpg')";
+    greeting.textContent = 'Good Morning,';
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon,';
+  } else {
+    // Evening
+    document.body.style.backgroundImage = "url('../img/evening.jpg')";
+    greeting.textContent = 'Good Evening,';
+    document.body.style.color = 'white';
+  }
+}
+
+// Get name.
+function getName() {
+  if (localStorage.getItem('name') == null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+// Set name.
+function setName(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed.
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+    console.log(e.target.innerText);
+  }
+}
+
+// Get focus.
+function getFocus() {
+  if (localStorage.getItem('focus') == null) {
+    focus.textContent = '[Enter Focus]';
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
+}
+
+// Set focus.
+// Set name.
+function setFocus(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed.
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+    console.log(e.target.innerText);
+  }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
 // Run.
 showTime();
+setBgGreet();
+getName();
+getFocus();
