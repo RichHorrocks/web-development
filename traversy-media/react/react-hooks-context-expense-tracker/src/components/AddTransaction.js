afb0eeccce1278134b8c40a25e1fe36f4e66d32b
Richard Horrocks <richardjhorrocks@gmail.com>
2020-02-26 08:27:06 +0000

    Addition of Context Provider.

--- traversy-media/react/react-hooks-context-expense-tracker/src/components/AddTransaction.js
+++ traversy-media/react/react-hooks-context-expense-tracker/src/components/AddTransaction.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add New Transaction</h3>
       <div className="form-control">
         <label htmlFor="text">Text</label>
-        <input type="text" placeholder="Enter text..." />
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+          placeholder="Enter text..."
+        />
       </div>
       <div className="form-control">
         <label htmlFor="amount">
           Amount <br />
           (negative - expense, positive - income)
         </label>
-        <input type="text" placeholder="Enter amount..." />
+        <input
+          type="text"
+          value={amount}
+          onChange={e => setAmount(e.target.value)}
+          placeholder="Enter amount..."
+        />
       </div>
       <button className="btn">Add transaction</button>
     </>
